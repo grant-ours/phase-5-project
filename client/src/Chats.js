dfe3e024ec8cb6dfff07c66c9117ed785a9ddf32
Grant Ours <grant.ours55@gmail.com>
2022-10-26 16:30:54 -0600

    Final css touches

--- client/src/Chats.js
+++ client/src/Chats.js
@@ -1,17 +1,25 @@
 import React from "react";
 import Chat from "./Chat";
 
-function Chats({chats}){
-    const chat = chats.map((chat) => {
-        return (
-            <Chat key={"chat" + chat.id}{...chat}/>
-        )
-    });
+function Chats({ chats }) {
+  const chatGroups = [];
+  let lastUserId;
+  chats.forEach((c) => {
+    if (c.user_id == lastUserId) {
+      chatGroups[chatGroups.length - 1].push(c);
+    } else {
+      chatGroups.push([c]);
+    }
+    lastUserId = c.user_id;
+  });
 
-    return (
-        <div>
-            {chat}
-        </div>
-    )
+  // const chatGroupElements = chatGroups.map(cg => {
+  //     <ChatGroup chats={cg}/>
+  // })
+  const chat = chats.map((chat) => {
+    return <Chat key={"chat" + chat.id} {...chat} />;
+  });
+
+  return <div>{chat}</div>;
 }
-export default Chats
+export default Chats;
