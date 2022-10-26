fab0e0b14172dda497fd368521c7ab0a4ba7937b
Grant Ours <grant.ours55@gmail.com>
2022-10-26 16:23:09 -0600

    Final css touches

--- client/src/index.js
+++ client/src/index.js
@@ -18,7 +18,7 @@ import actionCable from "actioncable";
 import 'semantic-ui-css/semantic.min.css'
 
 const CableApp = {};
-CableApp.cable = actionCable.createConsumer("ws://git.heroku.com/grantsdiscord.git/api/cable");
+CableApp.cable = actionCable.createConsumer("ws://grantsdiscord.herokuapp.com/api/cable");
 
 const router = createBrowserRouter(
   createRoutesFromElements(
