6594d0b405fbdfc64a041df18e425e3f4cab3d16
Grant Ours <grant.ours55@gmail.com>
2023-02-22 17:05:15 -0700

    remove proxy urls

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,18 +1,18 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-module.exports = function (app) {
-  app.use(
-    "/api/cable",
-    createProxyMiddleware({
-      target: "ws://localhost:3000",
-      ws: true,
-      changeOrigin: true,
-    })
-  );
-  app.use(
-    "/api",
-    createProxyMiddleware({
-      target: "http://localhost:3000",
-      changeOrigin: true,
-    })
-  );
-};
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+// module.exports = function (app) {
+//   app.use(
+//     "/api/cable",
+//     createProxyMiddleware({
+//       target: "ws://localhost:3000",
+//       ws: true,
+//       changeOrigin: true,
+//     })
+//   );
+//   app.use(
+//     "/api",
+//     createProxyMiddleware({
+//       target: "http://localhost:3000",
+//       changeOrigin: true,
+//     })
+//   );
+// };
